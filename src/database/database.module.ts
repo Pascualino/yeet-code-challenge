1df1cfbf9b523eac9fdeff8fe50d008b07956da0
Angel Pina <angel@paella.dev>
2025-11-02 15:55:58 +0100

    Removed some default env variables

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -13,11 +13,11 @@ export const DATABASE_CONNECTION = 'DATABASE_CONNECTION';
       provide: DATABASE_CONNECTION,
       useFactory: (configService: ConfigService) => {
         const pool = new Pool({
-          host: configService.get<string>('DATABASE_HOST', 'localhost'),
-          port: configService.get<number>('DATABASE_PORT', 5432),
-          user: configService.get<string>('DATABASE_USER', 'postgres'),
-          password: configService.get<string>('DATABASE_PASSWORD', 'postgres'),
-          database: configService.get<string>('DATABASE_NAME', 'yeet_casino'),
+          host: configService.get<string>('DATABASE_HOST'),
+          port: configService.get<number>('DATABASE_PORT'),
+          user: configService.get<string>('DATABASE_USER'),
+          password: configService.get<string>('DATABASE_PASSWORD'),
+          database: configService.get<string>('DATABASE_NAME'),
         });
 
         return drizzle(pool, { schema });
